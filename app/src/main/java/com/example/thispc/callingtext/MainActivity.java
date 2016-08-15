7e81f0a0e4a8b365b75a2d9f1999ff118aec31fe
pkarira <pulkit5208@gmail.com>
2016-08-16 01:00:12 +0530

    added function for sending message to server

--- app/src/main/java/com/example/thispc/callingtext/MainActivity.java
+++ app/src/main/java/com/example/thispc/callingtext/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.thispc.callingtext;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.BaseColumns;
@@ -29,15 +31,19 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 private final int REQUEST_CODE=1;
-    EditText editText;
+    EditText editText1;
+    EditText editText2;
+    String yourNumber;
+    String receiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        editText=(EditText)findViewById(R.id.editText2);
+        editText1=(EditText)findViewById(R.id.editText2);
+        editText2=(EditText)findViewById(R.id.editText);
         setSupportActionBar(toolbar);
-
+        yourNumber="7248187747";
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,9 +80,10 @@ public void OK(View v)
     }
     public void call(View v)
     {
-        if(editText.getText().toString()!=null)
+        if(editText1.getText().toString()!=null)
         {
-            call(editText.getText().toString());
+            receiver=editText1.getText().toString();
+            call(editText1.getText().toString());
         }else
             Toast.makeText(getApplicationContext(),"Please Enter Number",Toast.LENGTH_SHORT).show();
     }
@@ -88,6 +95,34 @@ public void OK(View v)
         catch (android.content.ActivityNotFoundException ex){
             Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();}
 
+    }
+    public void send(View v)
+    {
+        if(haveNetworkConnection()==true){
+            if(editText2.getText().toString()!=null)
+            {
+                BackGroundWorker b=new BackGroundWorker(this,2);
+                b.execute(yourNumber,receiver,editText2.getText().toString());
+            }else
+                Toast.makeText(getApplicationContext(),"please type message",Toast.LENGTH_SHORT).show();
+           }else
+            Toast.makeText(getApplicationContext(),"you have no internet connection",Toast.LENGTH_SHORT).show();
+    }
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
    /* @Override
     protected void onActivityResult(int requestCode, int resultCode,
